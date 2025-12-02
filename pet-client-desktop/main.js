14dd6916d94a7b55f0d68fb0514f7e3892d0db5a
DutulescuReli <dutulescureli14@gmail.com>
2025-12-02 20:14:46 +0200

    Add Electron desktop client (pet-client-desktop)

---
+++ pet-client-desktop/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  // DOAR DEV – încarcă aplicația client care rulează pe Vite
+  win.loadURL('http://localhost:5174'); // dacă Vite pornește pe alt port, îl schimbăm
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
